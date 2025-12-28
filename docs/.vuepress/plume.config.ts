239b7a3b0f82071f78a988d81567a7d90a4fa61e
Jursin <jursin@126.com>
2025-12-28 22:07:51 +0800

    更新主题，优化部分样式

--- docs/.vuepress/plume.config.ts
+++ docs/.vuepress/plume.config.ts
@@ -40,7 +40,7 @@ export default defineThemeConfig({
   /* 站点页脚 */
   footer: {
     message: 'Powered by <a target="_blank" href="https://v2.vuepress.vuejs.org/">VuePress</a> & <a target="_blank" href="https://theme-plume.vuejs.press">vuepress-theme-plume</a> | Copyright © 2025 Jursin',
-    copyright: '<img src="https://beian.miit.gov.cn/img/bg_logo.2ce2f33d.png" width="14" height="14" style="margin-right: 5px; display: inline-block; vertical-align: middle;"><a href="http://beian.miit.gov.cn/" target="_blank" >鄂ICP备2025157296号-1</a> | <img src="https://beian.mps.gov.cn/web/assets/logo01.6189a29f.png" width="14" height="14" style="margin-right: 5px; display: inline-block; vertical-align: middle;"><a href="https://beian.mps.gov.cn/#/query/webSearch" target="_blank">鄂公网安备42100302000228号</a>',
+    copyright: '<img src="/icon/miit.png" width="14" height="14" style="margin-right: 5px; display: inline-block; vertical-align: middle;"><a href="http://beian.miit.gov.cn/" target="_blank" >鄂ICP备2025157296号-1</a> | <img src="/icon/mps.png" width="14" height="14" style="margin-right: 5px; display: inline-block; vertical-align: middle;"><a href="https://beian.mps.gov.cn/#/query/webSearch" target="_blank">鄂公网安备42100302000228号</a>',
   },
 
   navbar,
